c752726519328b84ae826ab4a904c18f78b5cd14
Kristen Peck <kpeck71@gmail.com>
2017-09-10 23:36:24 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  my_array = ["hello","hi","hieee","hey gurl"]
+  my_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
