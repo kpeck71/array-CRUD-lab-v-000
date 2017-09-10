877eeee27836d3eb57c91efcf6781bf37afaa65f
Kristen Peck <kpeck71@gmail.com>
2017-09-10 23:38:25 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   my_array = ["hello","hi","hieee","hey gurl"]
-  my_array(2)
+  my_array[2]
 end
 
 def retrieve_first_element_from_array(array)
