f7d1ab55a38abd8a6946290af73c678cd9ad01db
Kristen Peck <kpeck71@gmail.com>
2017-09-10 23:35:39 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,6 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
+ my_array = ["hello","hi","hieee","hey gurl"]
  my_array.unshift("arrays!")
 end
 
