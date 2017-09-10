7d9fdd33846841a068fd5023bc3211dbc2c9937b
Kristen Peck <kpeck71@gmail.com>
2017-09-10 23:34:59 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   my_array = ["hello","hi","hieee","hey gurl"]
-  my_array << "sup"
+  my_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
